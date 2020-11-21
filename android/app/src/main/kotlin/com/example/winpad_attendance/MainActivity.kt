c2fd32886f0b27206571bf324da47cd4e4c83dce
firdausjuanda <firdausjuanda06@gmail.com>
2020-11-21 09:03:45 +0700

    start project

---
+++ android/app/src/main/kotlin/com/example/winpad_attendance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.winpad_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text('Error');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Text("Completed");
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading");
+      },
+    );
+  }
+}
